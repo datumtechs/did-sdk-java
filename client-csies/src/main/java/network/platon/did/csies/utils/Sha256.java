fee46d8b804835b9c03185525772646d5baa878f
jatel <jatelmomo@hotmail.com>
2022-08-02 18:08:10 +0800

    modify DID sdk

---
+++ client-csies/src/main/java/network/platon/did/csies/utils/Sha256.java
@@ -0,0 +1,46 @@
+package network.platon.did.csies.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Sha256 {
+
+    public static BigInteger byteToUin64(byte[] input){
+
+        byte[] array = new byte[8];
+        if(input.length > 8){
+            System.arraycopy(input, 0, array, 0, array.length);
+        }else{
+            array = input;
+        }
+
+        BigInteger m = new BigInteger(1, array);
+
+        return m;
+    }
+
+    public static byte[] uint64ToByte(BigInteger input){
+
+        byte[] array = input.toByteArray();
+        if(array.length < 8){
+            byte[] tmp = new byte[8];
+            System.arraycopy(array, 0, tmp, 8-array.length, array.length);
+            array = tmp;
+        }
+        return array;
+    }
+
+    public static byte[] sha256(byte[] input) {
+        byte[] result = null;
+        try {
+            MessageDigest  messageDigest = MessageDigest.getInstance("SHA-256");
+            result = messageDigest.digest(input);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
